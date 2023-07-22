1300381a283e8c4dc6dc53a6c12b2e6b98fd95c6
Muhammad Abdullah <abdullah.kiet@gmail.com>
2023-07-22 12:28:25 +0500

    UPDATED messages controller

--- messages/src/messages/dtos/messages.repository.ts
+++ messages/src/messages/dtos/messages.repository.ts
@@ -3,16 +3,19 @@ import { readFile, writeFile } from 'fs/promises';
 
 @Injectable()
 export class MessagesRepository {
+  //find one message
   async findOne(id: string) {
     const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
     return messages[id];
   }
+  //find all messages
   async findAll() {
     const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
     return messages;
   }
+  //create a message
   async create(content: string) {
     const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
