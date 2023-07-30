8ad259f47c2740beee3a597f84a269026e8a68bf
Muhammad Abdullah <abdullah.kiet@gmail.com>
2023-07-30 23:57:09 +0500

    added cpu and power modules and services

--- dependency_injection/src/cpu/cpu.service.ts
+++ dependency_injection/src/cpu/cpu.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
+//DI injectable
 @Injectable()
-export class CpuService {}
+export class CpuService {
+  constructor(public powerService: PowerService) {}
+}
