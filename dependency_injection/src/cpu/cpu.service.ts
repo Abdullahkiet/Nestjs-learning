b9021a9f0b12244ade497280c79511c59fa2191b
Muhammad Abdullah <abdullah.kiet@gmail.com>
2023-07-31 15:10:45 +0500

    completed section 6 DI project

--- dependency_injection/src/cpu/cpu.service.ts
+++ dependency_injection/src/cpu/cpu.service.ts
@@ -5,4 +5,10 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(public powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts of power from PowerService');
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
 }
