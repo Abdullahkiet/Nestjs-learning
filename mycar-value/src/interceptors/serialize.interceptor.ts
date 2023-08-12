6a279aa9a8de432818ded88239cacca2717ceebd
Muhammad Abdullah <abdullah.kiet@gmail.com>
2023-08-12 23:44:12 +0500

    updated serializer

--- mycar-value/src/interceptors/serialize.interceptor.ts
+++ mycar-value/src/interceptors/serialize.interceptor.ts
@@ -5,6 +5,7 @@ import { map } from 'rxjs/operators';
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     console.log('I am running before the handler', context);
+    console.log('I am running before the handler', context);
 
     return handler.handle().pipe(
       map((data: any) => {
