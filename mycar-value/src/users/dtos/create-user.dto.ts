dcce01cacb0d5cb23c15fa3c08d60972dff65ec3
Muhammad Abdullah <abdullah.kiet@gmail.com>
2023-08-12 23:46:48 +0500

    Update create-user.dto.ts

--- mycar-value/src/users/dtos/create-user.dto.ts
+++ mycar-value/src/users/dtos/create-user.dto.ts
@@ -2,8 +2,12 @@ import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
   //Validation decorators
+
+  //Validate email
   @IsEmail()
   email: string;
+
+  //Validate password
   @IsString()
   password: string;
 }
